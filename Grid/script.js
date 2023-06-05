2d30e5a0bf405f7c6f326020521d03e8ff6f9230
Serg <serega101617@gmail.com>
2023-06-05 09:33:59 +0200

    added js

--- Grid/script.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-let a = 3 * 2 ** 3 * 3;
-alert(a);
---
+++ JS/script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// let a = 3 * 2 ** 3 * 3;
+// alert(a);
+
+// let a = 'lsdhaskjflhg'; ----длинна строки
+// console.log(a.length);
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = 'aaa ' + str1 + ' bbb ' + str2 + ' ccc';  --шаблонные строки
+// let a = `'a'
+// 'b'
+// 'c'`;
+// console.log(a);
+// console.log(`aaa${str1} bbb ${str2} ccc`)
+
+// let a;
+// console.log(a);
+// let b = null;
+// console.log(b);
+// let q = false;
+// let w = true;
+// console.log(q, w);            ---------специальные значения;
+// let e = 'sdf';
+// let r = '3';
+// console.log(e * r);
+// console.log(10 / 0);
+// console.log(-5 / 0);
+
+// let a = '2' + 3;
+// console.log(a);                 = 23
+// let b = '1' + (2 + 3);
+// console.log(b);                 ==15  ----автоматическое преобразование;
+// let n = '5s' + '2';
+// console.log(n);                 ==5s2
+// let a = '' + 3 + 1;
+// alert(a);
+
+// let num = parseInt('12.6px');                ------выделение чисел в JS
+// console.log(num);
+// let a = parseFloat('12.6px');
+// console.log(a);
+
+// let a = '5px';
+// let b = '6px';
+// console.log(parseInt(a) + parseInt(b));
+
+// let a = parseFloat('5.5px');
+// let b = parseFloat('6.75px');
+// console.log(a + b);
+// let a = parseFloat('5.5px');
+// let b = parseFloat('6.75px');                ------выделение чисел в JS
+// console.log(a + b + 'px');
+
+// let a = 3;
+// let b = 4;
+// console.log(String(a) + String(b));     ---------Преобразование к строке;
+
+// let a = 2234532;
+// console.log(String(a).length);
+
+// let s = 2435233446;                                     -----Преобразование строк;
+// let v = 39768975;
+// console.log(String(s).length + String(v).length);
+
+// alert(String(true) + 1);                               ----Преобразование логического типа
+// alert(String(true) + Number(true));
+
+// let a = 'abcde';
+// console.log(a[0]);
+// console.log(a[2]);
+// console.log(a[4]);
+// ========
+// let a = 'abcde';
+// let b = a[4] + a[3] + a[2] + a[1] + a[0];  ----Получение символов строки
+// console.log(b);
+// ========
+// let v = 'abcde';
+// let s = 4;
+// console.log(v[s]);
+
+// let a = 'sdfgarfgf';
+// console.log(a[a.length - 1]);
+// let b = 'sdfgarfgf2353f';
+// console.log(b[b.length - 2]);
+// let c = 'sdfgarfgfdsa788906yhjdfsa'; ----------------Получение символа строки
+// console.log(c[c.length - 3]);
+
+// let a = '213512';
+// console.log(+a[0] + +a[1] + +a[2] + +a[3] + +a[4] + +a[5]); -------Строки с цифрами
+// console.log(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]) + Number(a[5]));
