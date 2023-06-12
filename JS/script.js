e6f902a62cf26b0f766146e020ab3da0d5b25885
Serg <serega101617@gmail.com>
2023-06-12 08:53:02 +0200

    start project uber

--- JS/script.js
+++ JS/script.js
@@ -45,12 +45,12 @@
 // let a = '5px';
 // let b = '6px';
 // console.log(parseInt(a) + parseInt(b));
-
+// ------выделение чисел в JS
 // let a = parseFloat('5.5px');
 // let b = parseFloat('6.75px');
 // console.log(a + b);
 // let a = parseFloat('5.5px');
-// let b = parseFloat('6.75px');                ------выделение чисел в JS
+// let b = parseFloat('6.75px');
 // console.log(a + b + 'px');
 
 // let a = 3;
@@ -90,3 +90,157 @@
 // let a = '213512';
 // console.log(+a[0] + +a[1] + +a[2] + +a[3] + +a[4] + +a[5]); -------Строки с цифрами
 // console.log(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]) + Number(a[5]));
+
+// let a = 0.1 + 0.2;
+// console.log(a.toFixed(3));
+// let b = 0.3 - 0.1;            ---------Метод toFixed округляет дробные числа, преобразует в строку!!
+// console.log(+b.toFixed(4) + 5);
+
+// alert('Ваше имя: ' + prompt('Ваше имя?'));
+// const a = prompt('Name');                       ------Prompt
+// alert('Your name is ' + a + '!');
+
+// let str = 'text';
+// document.write('<i>' + str + '</i>');  ---------------Запись в HTML
+// document.write(1 + '<br />' + 2 + '<br />' + 3 + '<br />' + 4 + '<br />' + 5);
+
+// let arr = ['a', 'b', 'c', 'd'];
+// arr[0] = arr[0] + 1;
+// arr[1] = arr[1] + 2;        -----Дозапись в элементы массива
+// arr[2] = arr[2] + 3;
+// console.log(arr);
+
+// let arr = [];
+// arr[0] = '1';
+// arr[1] = 't';
+// arr[2] = true;
+// console.log(arr);         -----Дозапись в элементы массива
+// let arr = [1, 2, 3];
+// arr[3] = 4;
+// arr[4] = 5;
+// console.log(arr);
+
+// const arr = [];
+// arr[3] = 'a';                -------добавление элементов в пустой массив
+// arr[8] = 'b';
+// console.log(arr.length);
+
+// let arr = [1, 2, 3];
+// arr.push(4, 5);
+// console.log(arr);
+
+// let arr = ['a', 'b', 'c'];
+// let key = 2;                        -----Получение элементов массива через переменную
+// console.log(arr[key]);
+
+// let arr = [1, 2, 3, 4, 5];
+// let key1 = 1;                           Сумма элементов массива через переменные
+// let key2 = 2;
+// console.log(arr[key1] + arr[key2]);
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// delete arr[1];
+// delete arr[3];                              Удаление элементов массива
+// console.log(arr);
+
+// let obj = { 1: 'a', 2: 'b', 3: 'c' }; -----------Объект с выводом его ключей
+// console.log(obj[1], obj[2]);
+
+// const obj = {
+//   name: 'Serg',                      -----------Объект с выводом его ключей, в ковычках!
+//   surname: 'xxx',
+//   patronymic: 'Bor',
+// };
+// console.log(obj['name'], obj['surname'], obj['patronymic']);
+
+// const obj = {
+//   year: 2023,
+//   month: 'June',               -----------Объект с выводом его ключей, без ковычек!
+//   day: 'Wensday'
+// }
+// console.log(obj.year, obj.month, obj.day);
+
+// let obj = {
+//   '1a': 1,
+//   b2: 2,
+//   'с-с': 3,
+//   'd 4': 4,
+//   e5: 5,
+// };
+// console.log(obj['1a']);      -------не все элементы Объекта можно писать без ковычек
+// console.log(obj.b2);
+// console.log(obj['c-c']);
+// console.log(obj['d 4']);
+// console.log(obj.e5);
+
+// let obj = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+// obj.b = 'r';
+// console.log(obj);
+// let obj = {x: 1, y: 2, z: 3};   --------работа с элементами объекта
+// obj.x **= 2;
+// obj.y **=2;
+// obj.z **= 2;
+// console.log(obj)
+
+// let obj = {};
+// obj['a'] = 1;
+// obj['b'] = 2;               --------добавление новых элементов в объект
+// obj['c'] = 3;
+// console.log(obj)
+
+// let obj = {}
+// obj.a = 23;                 --------добавление новых элементов в объект
+// obj.b = 'sd';
+// obj.new = 242;
+// console.log(obj)
+
+// let obj = {a: 1, b: 2, c: 3};
+// let k = Object.keys(obj);           -----Получение массива ключей объекта
+// console.log(k)
+
+// let obj = {a: 1, b: 2, c: 3};
+// console.log(Object.keys(obj).length);   -----Длина объекта
+
+// let obj = {x: 1, y: 2, z: 3};
+// let a = 'y'                         --------ключ объекта
+// console.log(obj[a]);
+// let obj = { x: 1, y: 2, z: 3 };
+// console.log(obj['x']);
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+// console.log(obj[key]);
+
+// ЦИКЛЫ
+// let arr = [1, 2, 3, 4, 5];
+// let b = [5, 0, 34];             -------------Перебор массива циклом for-of
+// for (let a of arr) {
+//   b.push(a);
+// }
+// console.log(b);
+
+// let obj = { x: 1, y: 2, z: 3 };
+// for (let a in obj) {                -----------Перебор объекта циклом for-in
+//   console.log(a);
+// }
+
+// let obj = { x: 1, y: 2, z: 3 };
+// for (let a in obj) {
+//   console.log(obj[a]);
+// }
+
+// let num = 3;
+// while (num < 90) {		-------Цикл while
+//   num *= 3;
+// }
+// console.log(num);
+
+for (let i = 10; i < 1000; i++) {
+  let str = String(i);
+  if (str[0] == 1) {
+    console.log(str[i]);
+  }
+}
---
+++ js practice/script.js
@@ -0,0 +1,104 @@
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length - 1]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[1] + arr[2] + arr[3] + arr[4] + arr[0]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log([arr.length]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// let a = +prompt('Number');
+// let b = +prompt('Number');
+// console.log(a % b);
+
+// let a = 'sldfjhgsdlkjf';
+// for (let i = a.length; i >= 0; i--) {
+//   console.log(a[i]);
+// }
+
+// Дан массив с числами. Найдите сумму квадратов элементов этого массива.
+// const arr = [2, 4, 5, 3];
+// let a = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   a += arr[i] ** 2;
+// }
+// console.log(a);
+
+// Дан массив с числами. Найдите сумму квадратных корней элементов этого массива.
+// const arr = [4, 3, 7, 9];
+// let a = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   a += Math.sqrt(arr[i]);
+// }
+// console.log(a);
+
+// Дан массив с числами. Найдите сумму положительных элементов этого массива.
+// const arr = [4, 3, -7, 9, -3, -6, 5, 9];
+// let a = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     a += arr[i];
+//   }
+// }
+// console.log(a);
+
+// Дан массив с числами. Найдите сумму тех элементов этого массива, которые больше нуля и меньше десяти.
+// const arr = [4, 3, -7, 9, -3, -6, 5, 9, 53, 42, 19, 4, 8];
+// let a = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0 && arr[i] < 10) {
+//     a += arr[i];
+//   }
+// }
+// console.log(a);
+
+// Дана строка:       // 'abcde'      Получите массив букв этой строки.
+// const str = 'abcde';
+// let a = [];
+// for (let i = 0; i < str.length; i++) {
+//   a.push(str[i]);
+// }
+// console.log(a);
+
+// Дано некоторое число:       12345     Получите массив цифр этого числа.
+// const a = String(12345);
+// let arr = [];
+// for (let i = 0; i < a.length; i++) {
+//   arr.push(+a[i]);
+// }
+// console.log(arr);
+
+// Дано некоторое число:      12345      Найдите сумму цифр этого числа.
+// const a = String(12345);
+// let b = 0;
+// for (let i = 0; i < a.length; i++) {
+//   b += +a[i];
+// }
+// console.log(b);
+
+// Заполните массив целыми числами от 1 до 10.
+// let arr = [];
+// for (let i = 1; i <= 10; i++) {
+//   arr.push(i);
+// }
+// console.log(arr);
+
+// Заполните массив четными числами из промежутка от 1 до 100.
+// let arr = [];
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     arr.push(i);
+//   }
+// }
+// console.log(arr);
+
+// Округлите эти дроби до одного знака в дробной части.
+// let a = [1.456, 2.125, 3.32, 4.1, 5.34];
+// let b = [];
+// for (let i = 0; i < a.length; i++) {
+//   b.push(a[i].toFixed(1));
+// }
+// console.log(b);
